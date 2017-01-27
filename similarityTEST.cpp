37a37d2f9e203b33d16e2fe12f8189df44e617e7
vauhochzett <v.zie@outlook.de>
2017-01-27 17:25:46 +0900

    Work on producing results; printing trajectory to console

--- similarityTEST.cpp
+++ similarityTEST.cpp
@@ -81,6 +81,9 @@ int main(int argc, char **argv)
     int max_frames = capture.get(CV_CAP_PROP_FRAME_COUNT);
     Mat last_T;
 
+    // DEBUG
+    string outp = "";
+
     while(true) {
         capture >> current;
 
@@ -97,7 +100,17 @@ int main(int argc, char **argv)
         vector <float> err;
 
         goodFeaturesToTrack(previous_grey, prev_corner, 200, 0.01, 30);
-        calcOpticalFlowPyrLK(previous_grey, current_grey, prev_corner, cur_corner, status, err);
+        try
+        {
+            calcOpticalFlowPyrLK(previous_grey, current_grey, prev_corner, cur_corner, status, err);
+        }
+        catch (Exception e)
+        {
+            cout << outp << endl;
+            cout << "Exception caught at calcOpticalFlowPyrLK, skipping! " << e.what() << endl;
+            k++;
+            break;
+        }
 
         // weed out bad matches
         for(size_t i=0; i < status.size(); i++) {
@@ -108,10 +121,20 @@ int main(int argc, char **argv)
         }
 
         // translation + rotation only
-        Mat T = estimateRigidTransform(prev_corner2, cur_corner2, false); // false = rigid transform, no scaling/shearing
+        Mat T;
+        try
+        {
+            T = estimateRigidTransform(prev_corner2, cur_corner2, false); // false = rigid transform, no scaling/shearing
 
-        // in rare cases no transform is found. We'll just use the last known good transform.
-        if(T.data == NULL) {
+            // in rare cases no transform is found. We'll just use the last known good transform.
+            if(T.data == NULL) {
+                last_T.copyTo(T);
+            }
+        }
+        catch (Exception e)
+        {
+            cout << outp << endl;
+            cout << "Exception caught at estimateRigidTransform, skipping! " << e.what() << endl;
             last_T.copyTo(T);
         }
 
@@ -129,7 +152,10 @@ int main(int argc, char **argv)
         current.copyTo(previous);
         current_grey.copyTo(previous_grey);
 
-        cout << "Frame: " << k << "/" << max_frames << " - good optical flow: " << prev_corner2.size() << endl;
+        //cout << "Frame: " << k << "/" << max_frames << " - good optical flow: " << prev_corner2.size() << endl;
+
+        outp = "Past frame: " + to_string(k) + "/" + to_string(max_frames) + " - good optical flow: " + to_string(prev_corner2.size());
+
         k++;
     }
 
@@ -151,117 +177,9 @@ int main(int argc, char **argv)
 
         out_trajectory << (i+1) << " " << x << " " << y << " " << a << endl;
 
-        // TEMP: Print the trajectory aka "difference"
-        printf("Trajectory: %f, %f, %f; Sum: %f", x, y, a, x+y+a);
+        // Print the trajectory aka "difference"
+        cout << i+1 << " | Trajectory: " << x << ", " << y << ", " << a << "; Sum: " << abs(x)+abs(y)+abs(a) << endl;
     }
-
-    // Step 3 - Smooth out the trajectory using an averaging window
-//    vector <Trajectory> smoothed_trajectory; // trajectory at all frames
-//
-//    for(size_t i=0; i < trajectory.size(); i++) {
-//        double sum_x = 0;
-//        double sum_y = 0;
-//        double sum_a = 0;
-//        int count = 0;
-//
-//        for(int j=-SMOOTHING_RADIUS; j <= SMOOTHING_RADIUS; j++) {
-//            if(i+j >= 0 && i+j < trajectory.size()) {
-//                sum_x += trajectory[i+j].x;
-//                sum_y += trajectory[i+j].y;
-//                sum_a += trajectory[i+j].a;
-//
-//                count++;
-//            }
-//        }
-//
-//        double avg_a = sum_a / count;
-//        double avg_x = sum_x / count;
-//        double avg_y = sum_y / count;
-//
-//        smoothed_trajectory.push_back(Trajectory(avg_x, avg_y, avg_a));
-//
-//        out_smoothed_trajectory << (i+1) << " " << avg_x << " " << avg_y << " " << avg_a << endl;
-//    }
-
-    // Step 4 - Generate new set of previous to current transform, such that the trajectory ends up being the same as the smoothed trajectory
-//    vector <TransformParam> new_prev_to_cur_transform;
-//
-//    // Accumulated frame to frame transform
-//    a = 0;
-//    x = 0;
-//    y = 0;
-//
-//    for(size_t i=0; i < prev_to_cur_transform.size(); i++) {
-//        x += prev_to_cur_transform[i].dx;
-//        y += prev_to_cur_transform[i].dy;
-//        a += prev_to_cur_transform[i].da;
-//
-//        // target - current
-//        double diff_x = smoothed_trajectory[i].x - x;
-//        double diff_y = smoothed_trajectory[i].y - y;
-//        double diff_a = smoothed_trajectory[i].a - a;
-//
-//        double dx = prev_to_cur_transform[i].dx + diff_x;
-//        double dy = prev_to_cur_transform[i].dy + diff_y;
-//        double da = prev_to_cur_transform[i].da + diff_a;
-//
-//        new_prev_to_cur_transform.push_back(TransformParam(dx, dy, da));
-//
-//        out_new_transform << (i+1) << " " << dx << " " << dy << " " << da << endl;
-//    }
-
-    // Step 5 - Apply the new transformation to the video
-//    capture.set(CV_CAP_PROP_POS_FRAMES, 0);
-//    Mat T(2,3,CV_64F);
-//
-//    int vert_border = HORIZONTAL_BORDER_CROP * previous.rows / previous.cols; // get the aspect ratio correct
-//
-//    k=0;
-//    while(k < max_frames-1) { // don't process the very last frame, no valid transform
-//        capture >> current;
-//
-//        if(current.data == NULL) {
-//            break;
-//        }
-//
-//        T.at<double>(0,0) = cos(new_prev_to_cur_transform[k].da);
-//        T.at<double>(0,1) = -sin(new_prev_to_cur_transform[k].da);
-//        T.at<double>(1,0) = sin(new_prev_to_cur_transform[k].da);
-//        T.at<double>(1,1) = cos(new_prev_to_cur_transform[k].da);
-//
-//        T.at<double>(0,2) = new_prev_to_cur_transform[k].dx;
-//        T.at<double>(1,2) = new_prev_to_cur_transform[k].dy;
-//
-//        Mat cur2;
-//
-//        warpAffine(current, cur2, T, current.size());
-//
-//        cur2 = cur2(Range(vert_border, cur2.rows-vert_border), Range(HORIZONTAL_BORDER_CROP, cur2.cols-HORIZONTAL_BORDER_CROP));
-//
-//        // Resize cur2 back to current size, for better side by side comparison
-//        resize(cur2, cur2, current.size());
-//
-//        // Now draw the original and stablised side by side for coolness
-//        Mat canvas = Mat::zeros(current.rows, current.cols*2+10, current.type());
-//
-//        current.copyTo(canvas(Range::all(), Range(0, cur2.cols)));
-//        cur2.copyTo(canvas(Range::all(), Range(cur2.cols+10, cur2.cols*2+10)));
-//
-//        // If too big to fit on the screen, then scale it down by 2, hopefully it'll fit :)
-//        if(canvas.cols > 1920) {
-//            resize(canvas, canvas, Size(canvas.cols/2, canvas.rows/2));
-//        }
-//
-//        imshow("before and after", canvas);
-//
-//        //char str[256];
-//        //sprintf(str, "images/%08d.jpg", k);
-//        //imwrite(str, canvas);
-//
-//        waitKey(20);
-//
-//        k++;
-//    }
 
     return 0;
 }
