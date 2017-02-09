4f2be47bee5510df63d9c8ed6d18a4ccf7cebcf1
vauhochzett <v.zie@outlook.de>
2017-02-09 16:15:54 +0900

    Adds length field to ClusterInfo

--- similarityClusterer/ClusterInfo.cpp
+++ similarityClusterer/ClusterInfo.cpp
@@ -6,17 +6,21 @@
 #define DOGIMAGESTABILIZATION_CLUSTERINFO_CPP
 
 #include <string>
+#include <assert.h>
 
 struct ClusterInfo {
     std::string name = "INVALID";
     double beginMsec = -1;
     double endMsec = -1;
+    double length = -1;
 
     ClusterInfo() {};
     ClusterInfo(std::string _name, double _beginMsec, double _endMsec) {
         this->name = _name;
         this->beginMsec = _beginMsec;
         this->endMsec = _endMsec;
+        this->length = _endMsec - _beginMsec;
+        assert(this->length >= 0);
     }
 };
 
