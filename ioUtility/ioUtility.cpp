a8deef366a328612c31e47fe14c97597dc6f68fc
vauhochzett <v.zie@outlook.de>
2017-02-23 12:24:20 +0900

    Adds flag detection

--- ioUtility/ioUtility.cpp
+++ ioUtility/ioUtility.cpp
@@ -3,29 +3,38 @@
 //
 
 #include <iostream>
-#include <string>
 
 using std::cout;
 using std::endl;
 using std::string;
 
 /**
- * Main method.
+ * Main method. Allow for some operations, like creating new tag files
+ * or outputting data in specific formats
  */
 int main(int argc, char **argv) {
-    // Idea: Allow for some operations, like creating new tag files
-    // or outputting data in specific formats
+    bool csvMode = false;
+    bool tagMode = false;
 
     if (argc == 2 && string(argv[1]) == "--help") {
-        cout << "Not implemented" << endl;
+        cout << "Flags (only one at a time)" << endl
+             << "-c: Create csv file from data" << endl
+             << "-t: Read from / write to tag files" << endl;
         return 0;
     }
 
     // Check for flag and read if given
     string last_arg = string(argv[argc - 1]);
     if (last_arg.find("-") == 0) {
-        if (last_arg.find('X') != string::npos) {
-            throw("NOT IMPLEMENTED");
+        if (last_arg.find('c') != string::npos) {
+            csvMode = true;
+            cout << "CSV mode activated" << endl;
+        } else if (last_arg.find('t') != string::npos) {
+            tagMode = true;
+            cout << "Tag file mode activated" << endl;
         }
     }
+
+    cout << "IMPLEMENTATION MISSING" << endl;
+    return 1;
 }
